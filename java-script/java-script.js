6d5ec1b78ca760aedcdbd6e9085eb8a12fbbfccd
Tomas Fransoi <tomasfransoi@gmail.com>
2022-05-06 15:10:01 -0300

    java-script-vercion-1

--- java-script/java-script.js
+++ java-script/java-script.js
@@ -1,50 +1,100 @@
-let Alumnos = parseInt(prompt("ingrese cantidad de alumnos"));
-let CantidadNotas = parseInt(prompt("ingrese cantidad de notas"));
-let PromedioCurso = 0;
-let NumeroDeAlumno = 1;
-for (let i = 0; i < Alumnos; i = i + 1) {
-    let NotaFinal = 0;
-    for (let j = 0; j < CantidadNotas; j = j + 1) {
-        let nota = parseFloat(prompt("ingrese la nota del alumno " + NumeroDeAlumno + "(solo calificaciones del 1 al 10)"));
-        if (nota > 10) {
-            while (nota > 10){
-                nota = parseFloat(prompt("ingrese correctamente la nota del alumno " + NumeroDeAlumno + "(solo calificaciones del 1 al 10)"));
-            }
-        }
-        NotaFinal = NotaFinal + nota;
-    }
-    let Promedio = NotaFinal;
-    Promedio = Promedio / CantidadNotas;
-    console.log(Promedio);
-    alert("el promedio del alumno numero " + NumeroDeAlumno + " es " + Promedio);
-    if (Promedio < 5.5) {
-        console.log("desaprobado");
-        alert("desaprobado");
-    } else if (Promedio >= 5.5 && Promedio <= 7.5) {
-        console.log("bueno");
-        alert("bueno");
-    } else if (Promedio >= 7.5 && Promedio <= 9.5) {
-        console.log("muy bueno");
-        alert("muy bueno");
-    } else {
-        console.log("Exelente");
-        alert("Exelente");
-    }
-    PromedioCurso = PromedioCurso + Promedio;
-    NumeroDeAlumno = NumeroDeAlumno + 1;
+let SiNo = " ";
+let precio = 0;
+let cantidadDeProductos = 0;
+let producto = " ";
+while (SiNo.toUpperCase() != "SI"){
+let numeroDeProducto =parseInt(prompt("seleccione el numero de un producto \n 1-Calcos fantasia \n 2-Calcos personalizadas \n 3-Calcos para motos \n 4-Calcos para baterias \n 5-Calcos para lanchas \n 6-Bolso porta celular \n 7-Bolso bajo asiento \n 8-Cubre vaina \n 9-Pines para rayos \n 10-Protectores anti ralladura \n 11-Fundas fantasia \n 12-Fundas mtb"));
+if(numeroDeProducto > 12){
+do {
+    numeroDeProducto =parseInt(prompt("seleccione el numero de un producto correctamente porfavor \n 1-Calcos fantasia \n 2-Calcos personalizadas \n 3-Calcos para motos \n 4-Calcos para baterias \n 5-Calcos para lanchas \n 6-Bolsos porta celular \n 7-Bolso bajo asiento \n 8-Cubre vaina \n 9-Pines para rayos \n 10-Protectores anti ralladura \n 11-Fundas fantasia \n 12-Fundas mtb"));
+} while (numeroDeProducto > 12);
 }
-PromedioCurso = PromedioCurso / Alumnos;
-console.log(PromedioCurso);
-if (PromedioCurso < 5.5) {
-    console.log("desempreño del curso deficiente");
-    alert("desempreño del curso deficiente");
-} else if (PromedioCurso >= 5.5 && PromedioCurso <= 7.5) {
-    console.log("desempreño del curso bueno");
-    alert("desempreño del curso bueno");
-} else if (PromedioCurso >= 7.5 && PromedioCurso <= 9) {
-    console.log("desempreño del curso muy bueno");
-    alert("desempreño del curso muy bueno");
-} else {
-    console.log("desempreño del curso Exelente");
-    alert("desempreño del curso Exelente");
+switch (numeroDeProducto) {
+    case 1:
+        producto = "Calcos fantasia";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 150;
+        alert("El precio seria de " + precio + " pesos");
+        break;
+    case 2:
+        producto = "Calcos personalizadas";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 200;
+        alert("El precio seria de " + precio + " pesos");
+        break;
+    case 3:
+        producto = "Calcos para motos";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 140;
+        alert("El precio seria de " + precio + " pesos");
+    break;
+    case 4:
+        producto = "Calcos para baterias";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 130;
+        alert("El precio seria de " + precio + " pesos");
+    break;
+    case 5:
+        producto = "Calcos para lanchas";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 10450;
+        alert("El precio seria de " + precio + " pesos");
+    break;
+    case 6:
+        producto = "Bolsos porta celular";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 660;
+        alert("El precio seria de " + precio + " pesos");
+    break;
+    case 7:
+        producto = "Bolso bajo asiento";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 550;
+        alert("El precio seria de " + precio + " pesos");
+    break;
+    case 8:
+        producto = "Cubre vaina";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 250;
+        alert("El precio seria de " + precio + " pesos");
+    break;
+    case 9:
+        producto = "Pines para rayos";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 280;
+        alert("El precio seria de " + precio + " pesos");
+    break;
+    case 10:
+        producto = "Protectores anti ralladura";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 300;
+        alert("El precio seria de " + precio + " pesos");
+    break;
+    case 11:
+        producto = "Fundas fantasia";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 280;
+        alert("El precio seria de " + precio + " pesos");
+    break;
+    case 12:
+        producto = "Fundas mtb";
+        alert("usted a seleccionado el producto " + producto);
+        cantidadDeProductos = prompt("cuantos productos quiere");
+        precio = cantidadDeProductos * 650;
+        alert("El precio seria de " + precio + " pesos");
+    break;
 }
+SiNo = prompt("Es correcta esta compra de " + cantidadDeProductos + " " + producto + " por " + precio + " (responda porfavor con un si o no)");
+}
+alert("Su compra de " + cantidadDeProductos + " " + producto + " por " + precio + " pesos a sido confirmada \n Su pedido llegara a su casa en 15 dias");
