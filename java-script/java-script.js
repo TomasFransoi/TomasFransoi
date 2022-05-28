7381de01c90b3f998c14f9229e2879925b53ef7b
Tomas Fransoi <tomasfransoi@gmail.com>
2022-05-28 19:46:08 -0300

    JavaScriptV1.3

--- java-script/java-script.js
+++ java-script/java-script.js
@@ -6,20 +6,30 @@ class Producto {
         this.cantidad = cantidad;
     }
 } 
+function guardarEnLocal(key,valor) {
+    localStorage.setItem(key, JSON.stringify(valor))
+}
+function obtenerValor(key) {
+    let info = JSON.parse(localStorage.getItem(key));
+    return(info)
+}
+
+
 const sectionCarrito = document.getElementById("sectionCarrito");
 const sectionContenedor = document.getElementById("sectionProductos");
-let calcosFantasia = new Producto("Calcos Fantacia",150,1,0);
-let calcosPersonalizadas = new Producto("Calcos Personalizadas",200,2,0);
-let calcosParaMotos = new Producto("Calcos Para Motos",140,3,0);
-let calcosParaBaterias = new Producto("Calcos Para Baterias",130,4,0);
-let calcosParaLanchas = new Producto("Calcos Para Lanchas",10450,5,0);
-let bolsosPortaCelular = new Producto("Bolsos Porta Celular",660,6,0);
-let bolsoBajoAsiento = new Producto("Bolso Bajo Asiento",550,7,0);
-let cubreVaina = new Producto("Cubre Vaina",250,8,0);
-let pinesParaRayos = new Producto("Pines Para Rayos",280,9,0);
-let protectoresAntiRalladura = new Producto("Protectores Anti Ralladura",300,10,0);
-let fundasFantasia = new Producto("Fundas Fantasia",280,11,0);
-let fundasMtb = new Producto("Fundas Mtb",650,12,0);
+const main = document.getElementById("main")
+let calcosFantasia = new Producto("Calcos Fantacia",150,1,1);
+let calcosPersonalizadas = new Producto("Calcos Personalizadas",200,2,1);
+let calcosParaMotos = new Producto("Calcos Para Motos",140,3,1);
+let calcosParaBaterias = new Producto("Calcos Para Baterias",130,4,1);
+let calcosParaLanchas = new Producto("Calcos Para Lanchas",10450,5,1);
+let bolsosPortaCelular = new Producto("Bolsos Porta Celular",660,6,1);
+let bolsoBajoAsiento = new Producto("Bolso Bajo Asiento",550,7,1);
+let cubreVaina = new Producto("Cubre Vaina",250,8,1);
+let pinesParaRayos = new Producto("Pines Para Rayos",280,9,1);
+let protectoresAntiRalladura = new Producto("Protectores Anti Ralladura",300,10,1);
+let fundasFantasia = new Producto("Fundas Fantasia",280,11,1);
+let fundasMtb = new Producto("Fundas Mtb",650,12,1);
 const productos = [calcosFantasia,calcosPersonalizadas,calcosParaMotos,calcosParaBaterias,calcosParaLanchas,bolsosPortaCelular,bolsoBajoAsiento,cubreVaina,pinesParaRayos,protectoresAntiRalladura,fundasFantasia,fundasMtb];
 const carrito = [];
 let idCalculator = 1
@@ -27,29 +37,73 @@ for (const elements of productos) {
     let div = document.createElement("div");
     div.innerHTML=`<h3>${elements.nombre}</h3> <p> el precio del producto es ${elements.precio} </p> \n <button type="button" id=${idCalculator} >agregar al carrito</button>`;
     sectionContenedor.appendChild(div); 
+    
     idCalculator = idCalculator + 1;
 }
 let boton1 = document.getElementById("1");
-boton1.onclick = () =>{carrito.push(calcosFantasia)};
 let boton2 = document.getElementById("2");
-boton2.onclick = () =>{carrito.push(calcosPersonalizadas)};
 let boton3 = document.getElementById("3");
-boton3.onclick = () =>{carrito.push(calcosParaMotos)};
 let boton4 = document.getElementById("4");
-boton4.onclick = () =>{carrito.push(calcosParaBaterias)};
 let boton5 = document.getElementById("5");
-boton5.onclick = () =>{carrito.push(calcosParaLanchas)};
 let boton6 = document.getElementById("6");
-boton6.onclick = () =>{carrito.push(bolsosPortaCelular)};
 let boton7 = document.getElementById("7");
-boton7.onclick = () =>{carrito.push(bolsoBajoAsiento)};
 let boton8 = document.getElementById("8");
-boton8.onclick = () =>{carrito.push(cubreVaina)};
 let boton9 = document.getElementById("9");
-boton9.onclick = () =>{carrito.push(pinesParaRayos)};
 let boton10 = document.getElementById("10");
-boton10.onclick = () =>{carrito.push(protectoresAntiRalladura)};
 let boton11 = document.getElementById("11");
-boton11.onclick = () =>{carrito.push(fundasFantasia)};
 let boton12 = document.getElementById("12");
-boton12.onclick = () =>{carrito.push(fundasMtb)};
+let buttonsAgregar = [boton1,boton2,boton3,boton4,boton5,boton6,boton7,boton8,boton9,boton10,boton11,boton12];
+
+
+for (let index = 0; index < productos.length; index++) {
+    buttonsAgregar[index].onclick = () => {
+        carrito.push(productos[index]);
+        guardarEnLocal("carrito",carrito);
+        let div = document.createElement("div");
+        div.innerHTML=`<h3>${productos[index].nombre}</h3> <p> el precio del producto es ${productos[index].precio} </p>`
+        sectionCarrito.appendChild(div); 
+        idCalculator = idCalculator + 1;
+
+    }
+};
+divBoton = document.createElement("div");
+divBoton.innerHTML=`<button type="button" id=13>Vaciar carrito</button><button type="button" id=14>Comprar carrito</button>`;
+main.appendChild(divBoton);
+let botonVaciar = document.getElementById("13");
+let botonComprar = document.getElementById("14");
+const carritoLocal = obtenerValor("carrito");
+botonVaciar.onclick = () => {
+    let divs = document.querySelectorAll("#sectionCarrito div");
+    for (let index = 0; index < carrito.length; index++) {
+        divs[index].remove();
+    }
+    localStorage.removeItem("carrito");
+    carrito.splice(0);
+
+}
+
+botonComprar.onclick = () => {
+    let divs = document.querySelectorAll("#sectionCarrito div");
+    for (let index = 0; index < carrito.length; index++) {
+        divs[index].remove();
+    }
+    divCompra = document.createElement("h3");
+    divCompra.innerHTML=`Su compra a sido realizada con exito`;
+    sectionCarrito.appendChild(divCompra);
+    localStorage.removeItem("carrito")
+    carrito.splice(0)
+}
+
+carritoLocal.forEach(element => {
+    carrito.push(element)
+});
+
+
+for (let index = 0; index < carrito.length; index++) {
+    let div = document.createElement("div");
+    div.innerHTML=`<h3>${carrito[index].nombre}</h3> <p> el precio del producto es ${carrito[index].precio} </p>`
+    sectionCarrito.appendChild(div); 
+    main.appendChild(divBoton);
+    
+}
+
